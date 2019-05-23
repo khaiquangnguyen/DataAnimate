43862cb27e81fbff002d2bc75a5a31f0342d7171
Khai Nguyen <kqn001@bucknell.edu>
2019-05-23 12:22:16 -0700

    working

--- original/Timeline.js
+++ original/Timeline.js
@@ -1,22 +1,6 @@
 
 
-class Scene {
-    constructor(duration) {
-        this.duration = duration;
-        this.actors = [];
-    }
 
-    add_actor(actor, self = this) {
-        this.actors.push(actor);
-    }
-    remove_actor(actor, self = this) {
-        for (var i = 0; i < self.actors.length; i++) {
-            if (self.actors[i] === actor) {
-                arr.splice(i, 1);
-            }
-        }
-    }
-}
 
 
 class Track {
