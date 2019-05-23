943d66f473600afc197dffe393f00bcd1cf51139
KHAI NGUYEN <kqn001@bucknell.edu>
2019-05-22 22:21:06 -0700

    working

--- original/SimpleBarChart.js
+++ original/SimpleBarChart.js
@@ -1,6 +1,7 @@
 
-class Actor {
-    constructor(width, height) {
+class GraphicalObject {
+    constructor(width, height, name) {
+        this.name = name;
         this.total_width = width;
         this.total_height = height;
         this.x = 0;
@@ -8,43 +9,125 @@ class Actor {
         this.opacity = 0;
         this.margin = { top: 20, right: 20, bottom: 20, left: 20 };
         this.track = new Track(this);
+        this.data = null;
+        this.svg_container = null;
     }
+
+    get_default_parameters(self = this) {
+        return {
+            "width": {
+                "type": "int",
+                "range": [0, 2000]
+            },
+            "height": {
+                "type": "int",
+                "range": [0, 2000]
+            },
+            "x": {
+                "type": "int",
+                "range": [0, 2000]
+            },
+            "y": {
+                "type": "int",
+                "range": [0, 2000]
+            },
+            "opacity": {
+                "type": "float",
+                "range": [0, 1]
+            },
+            "margin_top": {
+                "type": "int",
+                "range": [0, 2000]
+            },
+            "margin_right": {
+                "type": "int",
+                "range": [0, 2000]
+            }
+            ,
+            "margin_left": {
+                "type": "int",
+                "range": [0, 2000]
+            },
+            "margin_bottom": {
+                "type": "int",
+                "range": [0, 2000]
+            },
+            "data": {
+                "type": "selector",
+                "range": null
+            }
+        }
+    }
+
+    set_width(width) {
+        this.total_width = width;
+    }
+
+    set_height(height) {
+        this.total_height = height;
+    }
+
+    set_x(x) {
+        this.x = x;
+    }
+
+    set_y(y) {
+        this.y = y;
+    }
+    set_opacity(opacity) {
+        this.opacity = opacity;
+    }
+
+    set_margin_top(i) {
+        this.margin.top = i;
+    }
+    set_margin_left(i) {
+        this.margin.left = i;
+    }
+    set_margin_right(i) {
+        this.margin.right = i;
+    }
+    set_margin_bottom(i) {
+        this.margin.bottom = i;
+    }
+
+    set_data(data_file) {
+        this.data = data_file;
+    }
+
+    update_graph(self = this) {
+        // remove the current svg container, and replace it with a new one with the new attributes
+    }
+
 }
 
-class SimpleBarChart extends Actor {
+class SimpleBarChart extends GraphicalObject {
     constructor(width = 500, height = 500) {
-        super(width, height);
-        this.name = "Simple_Bar_Chart";
-        this.svg_container = null;
+        super(width, height, "Simple_Bar_Chart");
         this.inner_graph = null;
         this.x_axis = null;
         this.y_axis = null;
         this.bars = null;
-        this.data = null;
         this.graph_width = this.total_width - this.margin.left - this.margin.right;
         this.graph_height = this.total_height - this.margin.top - this.margin.bottom;
-        this.x_axis_params = { scale: "", domain: "" }
-        this.y_axis_params = { scale: "", domain: "" }
+        this.x_scale = null;
+        this.y_scale = null;
+        this.x_data = null;
+        this.y_data = null;
+        this.columns = null;
+        this.graph_x = null;
+        this.graph_y = null;
         // construct the svg object container
     }
 
-
-
     import_data(self = this) {
-        d3.csv("sales.csv", function (error, data) {
+        d3.csv(this.data, function (error, data) {
             if (error) throw error;
             self.data = data;
-            self.construct_graph();
+            self.columns = Object.keys(self.data[0]);
         });
     }
 
-    select_x_domain(self = this, domain) {
-        this.x_axis_params.domain = domain;
-    }
-
-    select_y_domain(self = this, domain) {
-        this.y_axis_params.domain = domain;
-    }
     construct_svg(self = this) {
         self.svg_container = d3.select("body").append("svg")
             .attr("width", self.total_width)
@@ -55,46 +138,44 @@ class SimpleBarChart extends Actor {
             .attr("class", "inner_graph")
             .attr("transform",
                 "translate(" + self.margin.left + "," + self.margin.top + ")");
+        console.log(self.svg_container);
     }
 
-
     construct_x_axis(self = this) {
+        console.log(self.x_data);
         // add the x Axis
-        var x = d3.scaleBand()
+        self.graph_x = d3.scaleBand()
             .range([0, self.graph_width])
-            .padding(0.1);
+        self.graph_x.domain(self.data.map(function (d) { return d[self.x_data]; }));
         this.x_axis = self.inner_graph.append("g")
             .attr("class", "x_axis")
             .attr("transform", "translate(0," + self.graph_height + ")")
-            .call(d3.axisBottom(x));
+            .call(d3.axisBottom(self.graph_x));
+
 
     }
 
     construct_y_axis(self = this) {
-        var y = d3.scaleLinear()
+        self.graph_y = d3.scaleLinear()
             .range([self.graph_height, 0]);
         // add the y Axis
+        this.graph_y.domain([0, d3.max(self.data, function (d) { return d[self.y_data]; })]);
         this.y_axis = self.inner_graph.append("g")
             .attr("class", "y_axis")
-            .call(d3.axisLeft(y));
+            .call(d3.axisLeft(self.graph_y));
+
     }
     construct_graph(self = this) {
-
-        // Scale the range of the data in the domains
-        x.domain(self.data.map(function (d) { return d.salesperson; }));
-        y.domain([0, d3.max(self.data, function (d) { return d.sales; })]);
         // append the rectangles for the bar chart
         self.bars = self.inner_graph.selectAll("." + self.name + "_bar")
             .data(self.data)
             .enter().append("rect")
             .attr("class", self.name + "_bar")
-            .attr("x", function (d) { return x(d.salesperson); })
-            .attr("width", x.bandwidth())
-            .attr("y", function (d) { return y(d.sales); })
+            .attr("x", function (d) { return self.graph_x(d[self.x_data]); })
+            .attr("width", self.graph_x.bandwidth())
+            .attr("y", function (d) { return self.graph_y(d[self.y_data]); })
             .attr("fill", "blue")
-            .attr("height", function (d) { return self.graph_height - y(d.sales); });
-
-
+            .attr("height", function (d) { return self.graph_height - self.graph_y(d[self.y_data]); });
     }
 
     get_targetable_components(self = this) {
@@ -106,4 +187,40 @@ class SimpleBarChart extends Actor {
             "y_axis": self.y_axis
         }
     }
+
+    get_parameters(self = this) {
+        return {
+            "x_axis_scale": {
+                "type": "dropdown",
+                "range": ['scaleBand', 'scaleLinear']
+            },
+            "y_axis_scale": {
+                "type": "dropdown",
+                "range": ['scaleBand', 'scaleLinear']
+            },
+            "x_data": {
+                "type": "dropdown",
+                "range": this.columns
+            },
+            "y_data": {
+                "type": "dropdown",
+                "range": this.columns
+            },
+
+        }
+    }
+
+    set_x_data(x_data) {
+        this.x_data = x_data;
+    }
+    set_y_data(y_data) {
+        this.y_data = y_data;
+    }
+    set_x_scale(x_scale) {
+        this.x_scale = x_scale;
+    }
+    set_y_scale(y_scale) {
+        this.y_scale = y_scale;
+    }
+
 }
--- original/index.js
+++ original/index.js
@@ -1,26 +1,26 @@
 // create actor
 
-main_scene = new Scene(10000);
-bar_chart = new SimpleBarChart(500, 500);
-bar_chart.construct_svg();
-bar_chart.import_data();
-main_scene.add_actor(bar_chart);
+// main_scene = new Scene(10000);
+// bar_chart = new SimpleBarChart(500, 500);
+// bar_chart.construct_svg();
+// bar_chart.import_data();
+// main_scene.add_actor(bar_chart);
 
-effect_stack = new EffectStack(bar_chart, 500, 500);
-fadein = new FadeInEffect(bar_chart, effect_stack);
-second_effect_stack = new EffectStack(bar_chart, 1500, 500)
-fadeout = new FadeOutEffect(bar_chart, second_effect_stack);
-effect_stack.add_effect(fadein);
+// effect_stack = new EffectStack(bar_chart, 500, 500);
+// fadein = new FadeInEffect(bar_chart, effect_stack);
+// second_effect_stack = new EffectStack(bar_chart, 1500, 500)
+// fadeout = new FadeOutEffect(bar_chart, second_effect_stack);
+// effect_stack.add_effect(fadein);
 
-// second_effect_stack.add_effect(fadeout);
-bar_chart.track.add_effect_stack(effect_stack);
-// bar_chart.track.add_effect_stack(second_effect_stack);
+// // second_effect_stack.add_effect(fadeout);
+// bar_chart.track.add_effect_stack(effect_stack);
+// // bar_chart.track.add_effect_stack(second_effect_stack);
 
-console.log(fadeout);
-setTimeout(() => {
-    bar_chart.track.play(500);
-}, 100);
-console.log(bar_chart.track);
+// console.log(fadeout);
+// setTimeout(() => {
+//     bar_chart.track.play(500);
+// }, 100);
+// console.log(bar_chart.track);
 
 // const FadeInEffect = new FadeinEffect("FadeIn", chart, 1000);
 // const resize = new ResizeEffect(chart, 1000);
@@ -28,3 +28,19 @@ console.log(bar_chart.track);
 //     // FadeInEffect.executeEffect();
 //     resize.executeEffect();
 // }, 500);
+
+bar_chart = new SimpleBarChart(500, 500);
+bar_chart.set_data("sales.csv");
+bar_chart.import_data();
+setTimeout(() => {
+    console.log(bar_chart.get_parameters());
+    console.log(bar_chart);
+    bar_chart.set_x_data('salesperson')
+    bar_chart.set_y_data('sales')
+    bar_chart.construct_svg();
+    bar_chart.construct_x_axis();
+    bar_chart.construct_y_axis();
+    bar_chart.construct_graph();
+
+}, 100);
+
