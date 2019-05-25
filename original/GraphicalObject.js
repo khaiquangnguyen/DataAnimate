0254ad341ae119474b03d0420db5e98a6ef2bbca
KHAI NGUYEN <kqn001@bucknell.edu>
2019-05-25 16:40:10 -0700

    working

--- original/GraphicalObject.js
+++ original/GraphicalObject.js
@@ -15,7 +15,7 @@ class GraphicalObject {
         this.total_height = height;
         this.x = 100;
         this.y = 50;
-        this.opacity = 0;
+        this.opacity = 1;
         this.track = new Track(this);
         this.data = null;
         this.svg_container = null;
@@ -30,7 +30,7 @@ class GraphicalObject {
             .attr("height", this.total_height)
             .attr("x", this.x)
             .attr("y", this.y)
-            .attr("opacity", 0.2)
+            .attr("opacity", this.opacity)
             .attr("id", self.name);
         self.svg_container.attr('viewBox', `0 0 ${this.total_width} ${this.total_height}`)
             .attr('preserveAspectRatio', 'none')
@@ -44,13 +44,22 @@ class GraphicalObject {
         self.bounding_box.opacity(0.2);
         self.bounding_box.selectize().draggable().resize();
         self.bounding_box.hide();
+
         self.bounding_box.on('resizing', function (event) {
             console.log("resizing");
-            self.update_defaults(self.bounding_box.attr('x'), self.bounding_box.attr('y'), self.bounding_box.attr('width'), self.bounding_box.attr('height'));
+            var x = self.bounding_box.attr('x');
+            var y = self.bounding_box.attr('y');
+            var width = self.bounding_box.attr('width');
+            var height = self.bounding_box.attr('height');
+            self.update_defaults(x, y, width, height);
         });
         this.bounding_box.on('dragmove', function (event) {
             console.log("moving");
-            self.update_defaults(self.bounding_box.attr('x'), self.bounding_box.attr('y'), self.bounding_box.attr('width'), self.bounding_box.attr('height'));
+            var x = self.bounding_box.attr('x');
+            var y = self.bounding_box.attr('y');
+            var width = self.bounding_box.attr('width');
+            var height = self.bounding_box.attr('height');
+            self.update_defaults(x, y, width, height);
         });
 
         var target = SVG.adopt(document.getElementById(self.name));
@@ -61,73 +70,54 @@ class GraphicalObject {
 
     get_default_parameters(self = this) {
         return {
-            "name": {
-                "type": input_types.STRING,
-                "range": "",
-                "tooltips": "The name of the graphical object",
-                "setter": null,
+            name: {
+                type: input_types.STRING,
+                range: "",
+                tooltips: "The name of the graphical object",
+                value: this.name
             },
-            "width": {
-                "type": input_types.INT,
-                "range": [-Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER]
+            width: {
+                type: input_types.INT,
+                range: [-Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER],
+                value: this.total_width,
             },
-            "height": {
-                "type": input_types.INT,
-                "range": [-Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER]
+            height: {
+                type: input_types.INT,
+                range: [-Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER],
+                value: this.total_height,
             },
-            "x": {
-                "type": input_types.INT,
-                "range": [-Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER]
+            x: {
+                type: input_types.INT,
+                range: [-Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER],
+                value: this.x
             },
-            "y": {
-                "type": input_types.INT,
-                "range": [-Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER]
+            y: {
+                type: input_types.INT,
+                range: [-Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER],
+                value: this.y
             },
-            "opacity": {
-                "type": input_types.FLOAT,
-                "range": [0, 1]
+            opacity: {
+                type: input_types.FLOAT,
+                range: [0, 1],
+                value: this.opacity
             },
-            "data": {
-                "type": input_types.SELECTOR,
-                "range": null
-            }
+            data: {
+                type: input_types.SELECTOR,
+                range: null,
+                value: this.data
+            },
+            linked_object: this
         }
     }
 
-    set_name(name) {
-        this.name = name;
-    }
-    set_width(width) {
-        this.total_width = width;
-    }
-
-    set_height(height) {
-        this.total_height = height;
-    }
-
-    set_x(x) {
+    update_defaults(x = this.x, y = this.y, width = this.total_width, height = this.total_height, name = this.name, opacity = this.opacity) {
         this.x = x;
-        this.svg_container.attr("x", x);
-    }
-
-    set_y(y) {
         this.y = y;
-        this.svg_container.attr("y", y);
-
-    }
-    set_opacity(opacity) {
+        this.total_width = width;
+        this.total_height = height;
+        this.name = name;
         this.opacity = opacity;
-        this.svg_container.attr("opacity", opacity);
-    }
-
-
-    update_defaults(self = this, x = this.x, y = this.y, width = this.total_width, height = this.total_height, name = this.name, opacity = this.opacity) {
-        this.x = x;
-        this.y = y;
-        this.total_width = width;
-        this.total_height = height;
-        this.name = name;
-        self.svg_container
+        this.svg_container
             .attr("x", this.x)
             .attr("y", this.y)
             .attr("width", this.total_width)
